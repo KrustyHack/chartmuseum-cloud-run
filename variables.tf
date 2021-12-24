d364510f5d09093758b7a777562c930845ad5dc3
KrustyHack <KrustyHack@users.noreply.github.com>
2021-12-24 20:37:24 +0100

    Remove default variables, resolves #1, resolves #2

--- variables.tf
+++ variables.tf
@@ -1,36 +1,27 @@
 variable "project" {
   description = "GCP project"
-  default     = "YOURPROJECT"
 }
 variable "zone" {
-  description = "GCP zone"
-  default     = "europe-west1-d"
+  description = "GCP zone (e.g. : europe-west1-d)"
 }
 variable "region" {
-  description = "GCP region"
-  default     = "europe-west1"
+  description = "GCP region (e.g. : europe-west1)"
 }
 variable "chartmuseum_docker_image" {
-  description = "Docker image to deploy"
-  default     = "europe-west1-docker.pkg.dev/YOURPROJECT/docker-public/chartmuseum:xxx"
+  description = "Docker image to deploy (e.g : europe-west1-docker.pkg.dev/YOURPROJECT/docker-public/chartmuseum:xxx)"
 }
 variable "bucket_name" {
   description = "Name of the bucket where to store charts"
-  default     = "MYBUCKET"
 }
 variable "dns_managed_zone_name" {
   description = "DNS zone name where to setup Cloud Run custom domain"
-  default     = "MYZONE"
 }
 variable "dns_name" {
-  description = "Custom domain to service Cloud Run service"
-  default     = "myservice.mydomain.com"
+  description = "Custom domain to service Cloud Run service (e.g. : myservice.mydomain.com)"
 }
 variable "project_secret" {
   description = "Project where to lookup for secret injected in Cloud Run container"
-  default     = "YOURPROJECT"
 }
 variable "secret_name" {
   description = "Name of the secret to lookup and inject in Cloud Run container"
-  default     = "MYSECRET"
 }
