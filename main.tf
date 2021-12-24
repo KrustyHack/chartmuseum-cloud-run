6446652c7ed4fe7c7a74607c65fbd3300cf42f86
KrustyHack <KrustyHack@users.noreply.github.com>
2021-12-24 20:39:51 +0100

    Refactoring

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  # Doc for version constraint : https://www.terraform.io/docs/language/expressions/version-constraints.html#version-constraint-syntax
+  required_version = ">= 1.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~>3.71"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~>3.71"
+    }
+  }
+}
+
 provider "google" {
   project = var.project
   region  = var.region
